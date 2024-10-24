600c4f341c8dbc9b94e104781757530bc2f0a6cf
kamendrias <76965529+kamendrias@users.noreply.github.com>
2024-10-24 09:55:24 -0400

    icons and game manager started

---
+++ Timefall/Assets/Scripts/GameManager.cs
@@ -0,0 +1,93 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance;
+    public string currentScene = "Title";
+
+    public GameObject settingsPanel;
+
+    public bool isSettingsOpen = false;
+
+    // Awake is called when the script instance is being loaded.
+    void Awake()
+    {
+        // If there is an instance, and it's not me, delete myself.
+        if (Instance != null && Instance != this) 
+        { 
+            Destroy(this); 
+        } 
+        else 
+        { 
+            Instance = this; 
+            DontDestroyOnLoad(this.gameObject);
+        } 
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Starting Game Manager");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Debug.Log("Escape key was pressed");
+
+        }
+        
+    }
+
+    public void LoadTitleScene()
+    {
+        SceneManager.LoadScene("TitleScene", LoadSceneMode.Single);
+        this.currentScene = "Title";
+    }
+
+    public void LoadBattleScene()
+    {
+        SceneManager.LoadScene("BattleScene", LoadSceneMode.Single);
+        this.currentScene = "Battle";
+    }
+
+    public void LoadAcademyScene()
+    {
+        SceneManager.LoadScene("AcademyScene", LoadSceneMode.Single);
+        this.currentScene = "Academy";
+    }
+
+    public void LoadResearchScene()
+    {
+        SceneManager.LoadScene("ResearchScene", LoadSceneMode.Single);
+        this.currentScene = "Research";
+    }
+
+    public void ToggleBattleSettingsMenu()
+    {
+        if(isSettingsOpen)
+        {
+            settingsPanel.SetActive(false);
+            isSettingsOpen = false;
+        } else {
+            settingsPanel.SetActive(true);
+            isSettingsOpen = true;
+        }
+        
+    }
+
+    public void QuitApplication()
+    {
+#if UNITY_STANDALONE
+        Application.Quit();
+#endif
+#if UNITY_EDITOR 
+        UnityEditor.EditorApplication.isPlaying = false;
+#endif
+    }
+}
